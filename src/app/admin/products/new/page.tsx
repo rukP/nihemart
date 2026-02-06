5ce0908b51c86715434f9e13e00e2f871f42c890
RUKUNDO-Prince <rukundoprince951@gmail.com>
2026-02-06 15:58:02 +0200

    Initial commit

--- src/app/admin/products/new/page.tsx
+++
@@ -1,5 +0,0 @@
-import AddEditProductForm from "@/components/admin/add-edit-product-form";
-
-export default function AddProductPage() {
-  return <AddEditProductForm />;
-}
--- src/locales/index.ts
+++
@@ -1,9 +0,0 @@
-import { en } from "./en";
-import { rw } from "./rw";
-
-export const translations = {
-  en,
-  rw,
-};
-
-export type Language = keyof typeof translations;
